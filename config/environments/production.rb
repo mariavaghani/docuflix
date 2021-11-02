9455eea46ab75218ef017681929506b22dc4fccd
mariavaghani <maria.vaghani@gmail.com>
2021-11-01 22:24:23 -0400

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.active_storage.service = :amazon_prod
+  # trying this code >>>
+  config.default_url_options = { host: "https://docuflix.herokuapp.com" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
