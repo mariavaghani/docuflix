e702904b48215a77e8de77bd3d3101c2b0d31fae
mariavaghani <maria.vaghani@gmail.com>
2021-11-09 17:31:39 -0500

    push

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
   validates :genre, presence: true
   has_many :documentaries_genres, dependent: :destroy
-
+  puts "hi"
   has_many :documentaries, through: :documentaries_genres
 end
